4d9ec078f4de6c5286c5835baa18978cb032e400
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-05-13 22:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	if(!input_digit || input_digit.length === 0) return [];
+
+	const digitToLetters = {
+    '0': '0',
+    '1': '1',
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+	}
+	const result = [];
+	function backtrack(index, current) {
+     if(index === input_digit.length){
+		 result.push(current);
+		 return;
+	 }		
+		const letters = digitToLetters[input_digit[index]] || '' ;
+		for(let ch of letters){
+			backtrack(index + 1, current + ch);
+		}
+	}
+	backtrack(0, '');
+	return result.sort();
 }
 
 module.exports = letterCombinations;
